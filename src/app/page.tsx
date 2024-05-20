8b3903c78ad9b3f2cda973bcf5540c5457b0359b
dracu-lah <nevilnicks4321@gmail.com>
2024-05-20 11:40:49 +0530

    scaffold ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import Link from "next/link";
-
 export default function HomePage() {
+  const imageUrls = Array.from({ length: 200 }).map((_, index) => ({
+    id: index,
+    src: "https://utfs.io/f/cdd51216-d13b-4f59-b73a-d6918281d6a0-30awpa.NIGHT~2.jpg",
+  }));
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)
+    <main className="">
+      <div className=" flex w-full flex-wrap  justify-center gap-4">
+        {imageUrls.map((imageUrl) => (
+          <img
+            key={imageUrl.id}
+            src={imageUrl.src}
+            alt={imageUrl.id + ".png"}
+            className="h-32 w-56 rounded-sm border object-cover"
+          />
+        ))}
+      </div>
     </main>
   );
 }
